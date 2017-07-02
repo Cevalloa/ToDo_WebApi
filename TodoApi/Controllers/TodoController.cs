b49ced380ec4bcd148b9fb27c79e359165d6a412
Alex Cevallos <Alex.Cevallos@alaskaair.com>
2017-07-01 17:31:04 -0700

    Created Web Api Controller to handle GET Request of list

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    // Controller Class
+    [Route("api/Todo")]
+   public class TodoController: Controller
+    {
+        private readonly TodoContext _context;
+
+        // Dependency Injection to inject the database context
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            // Constructor adds item to in memory database if one doesn't exist
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(item);
+        }
+    }
+}
